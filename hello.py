93fad482ecfef9dc3778a95fabacce38907375ce
adillareef45 <adil.lareef56@gmail.com>
2026-06-15 17:57:45 +0400

    Adjusted variable names

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 # Read from environment
 api_key = os.environ.get('API_KEY')
-database = os.environ.get('DATABASE_NAME', 'default.db')
+database = os.environ.get('DATABASE_URL', 'default.db')
 
 print(f"Using database: {api_key}")
+print(f"Using database: {database}")
